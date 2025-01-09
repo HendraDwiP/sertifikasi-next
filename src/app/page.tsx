8dfa003b13b74148e0eaf29c8c08a0c578d46a6a
Hendra Dwi Prastia <hendrapang888@gmail.com>
2025-01-09 16:46:49 +0700

    revisi error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/Header"
 import Introduction from "@/components/Inroduction"
 import Tech from "@/components/Tech"
 
--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import TechCard from "./Techcard";
 
 export default function Tech() {
